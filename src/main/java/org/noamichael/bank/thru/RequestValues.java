2345edc4a1a185c8e1775a334841fae570ae8b06
Michael <Michael@Michael-PC>
2014-10-16 23:43:39 -0400

    New menu

--- src/main/java/org/noamichael/bank/thru/RequestValues.java
+++ src/main/java/org/noamichael/bank/thru/RequestValues.java
@@ -1,6 +1,7 @@
 package org.noamichael.bank.thru;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -8,6 +9,7 @@ import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.PieChartModel;
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 
@@ -22,33 +24,42 @@ public class RequestValues {
     private static final List<DefaultMenuItem> links = new ArrayList();
     private static final DefaultMenuModel defaultMenuModel = new DefaultMenuModel();
     private static final List<Entry> accounts = new ArrayList();
-    private static final BarChartModel model = new BarChartModel();
+    private static final BarChartModel barChart = new BarChartModel();
+    private static final PieChartModel pieChart = new PieChartModel();
 
     static {
         DefaultMenuItem dashboard = new DefaultMenuItem("Dashboard");
         dashboard.setCommand("#{sessionValues.setIndex(0)}");
-        
+        dashboard.setUrl("dashboard.xhtml");
+
         DefaultMenuItem setupPayment = new DefaultMenuItem("Setup Payment");
         setupPayment.setCommand("#{sessionValues.setIndex(1)}");
-                
+        setupPayment.setUrl("setupPayment.xhtml");
+
         DefaultMenuItem currentBalance = new DefaultMenuItem("Current Balance");
         currentBalance.setCommand("#{sessionValues.setIndex(2)}");
-        
+        currentBalance.setUrl("currentBalance.xhtml");
+
         DefaultMenuItem priorityPayment = new DefaultMenuItem("Priority Payment");
         priorityPayment.setCommand("#{sessionValues.setIndex(3)}");
-        
+        priorityPayment.setUrl("priorityPayment.xhtml");
+
         DefaultMenuItem reminders = new DefaultMenuItem("Reminders");
         reminders.setCommand("#{sessionValues.setIndex(4)}");
-        
+        reminders.setUrl("reminders.xhtml");
+
         DefaultMenuItem spendingPattern = new DefaultMenuItem("Spending Pattern");
         spendingPattern.setCommand("#{sessionValues.setIndex(5)}");
-        
+        spendingPattern.setUrl("spendingPattern.xhtml");
+
         DefaultMenuItem transfers = new DefaultMenuItem("Transfers");
         transfers.setCommand("#{sessionValues.setIndex(6)}");
-        
+        transfers.setUrl("transfers.xhtml");
+
         DefaultMenuItem logout = new DefaultMenuItem("Logout");
         logout.setCommand("logout");
-        
+        logout.setUrl("index.xhtml");
+
         defaultMenuModel.addElement(dashboard);
         defaultMenuModel.addElement(setupPayment);
         defaultMenuModel.addElement(currentBalance);
@@ -70,20 +81,35 @@ public class RequestValues {
         accounts.add(new Entry("2837772639", "$13.98"));
         accounts.add(new Entry("1414257899", "$3,216.23"));
         accounts.add(new Entry("1726362000", "$180.00"));
-        
-         ChartSeries amount = new ChartSeries();
-         amount.setLabel("Amount");
-         amount.set("Aug", 150.65);
-         amount.set("Sep", 345.12);
-         amount.set("Oct", 246.23);
-         model.addSeries(amount);
-         model.setTitle("Monthly Spending");
-         Axis xAxis = model.getAxis(AxisType.X);
-         xAxis.setLabel("Month");
-         Axis yAxis = model.getAxis(AxisType.Y);
-         yAxis.setLabel("Amount");
-         yAxis.setMin(0);
-         yAxis.setMax(1000);
+
+        ChartSeries amount = new ChartSeries();
+        amount.setLabel("Amount");
+        amount.set("Aug", 150.65);
+        amount.set("Sep", 345.12);
+        amount.set("Oct", 246.23);
+        barChart.addSeries(amount);
+        barChart.setTitle("Monthly Spending");
+        Axis xAxis = barChart.getAxis(AxisType.X);
+        xAxis.setLabel("Month");
+        Axis yAxis = barChart.getAxis(AxisType.Y);
+        yAxis.setLabel("Amount (Dollars)");
+        yAxis.setMin(0);
+        yAxis.setMax(1000);
+
+        pieChart.set("Entertainment", 540);
+        pieChart.set("Gas", 325);
+        pieChart.set("Food", 702);
+        pieChart.set("Misc.", 421);
+
+        pieChart.setTitle("Spending Areas");
+        pieChart.setLegendPosition("e");
+        pieChart.setShowDataLabels(true);
+        pieChart.setDiameter(130);
+
+    }
+    
+    public Date getToday(){
+        return new Date();
     }
 
     public List<Entry> getAccounts() {
@@ -100,8 +126,12 @@ public class RequestValues {
     public List<DefaultMenuItem> getLinks() {
         return links;
     }
-    
-    public BarChartModel getMonthlySpending(){
-        return model;
+
+    public BarChartModel getMonthlySpending() {
+        return barChart;
+    }
+
+    public PieChartModel getSpendingAreas() {
+        return pieChart;
     }
 }
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("body").fadeIn();
+    $(".slide-menu-item-container").mouseenter(function () {
+        $(this).addClass("slide-menu-item-container-hover");
+    });
+    $(".slide-menu-item-container").mouseleave(function () {
+        $(this).removeClass("slide-menu-item-container-hover");
+    });
 });
+function toggleMenu() {
+    $(".slide-menu").stop().slideToggle();
+}
 
 
